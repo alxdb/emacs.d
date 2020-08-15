e79d9667b597e910f5e06909bc018c1dbefa327a
Alexander Davidson Bryan <alxdb@protonmail.com>
2020-08-15 14:16:27 +0100

    added more essentials

--- custom.el
+++ custom.el
@@ -4,11 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes t)
+ '(default-frame-alist
+    (quote
+     ((vertical-scroll-bars)
+      (width . 120)
+      (height . 55))))
+ '(dired-listing-switches "-l")
  '(explicit-shell-file-name "/bin/zsh")
  '(inhibit-startup-screen t)
  '(initial-scratch-message nil)
  '(make-backup-files nil)
- '(package-selected-packages (quote (try evil nord-theme)))
+ '(package-selected-packages (quote (cider company form-feed magit evil nord-theme)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
--- init.el
+++ init.el
@@ -1,19 +1,43 @@
+;; custom functions
 (defun emacs-conf-file (filename)
   (concat user-emacs-directory filename))
 
-(defun open-init-file ()
+(defun adb/open-init-file ()
   (interactive)
   (find-file (emacs-conf-file "init.el")))
 
+;; seperate file for customizations
 (setq custom-file (emacs-conf-file "custom.el"))
 (load custom-file)
 
+;; add melpa to package repositories
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; theme
 (load-theme 'nord)
 
+;; evil
 (require 'evil)
 (evil-mode)
+
+;; magit
+(require 'magit)
+
+;; hooks
+(add-hook 'help-mode-hook 'form-feed-mode)
+(add-hook 'compilation-mode-hook 'form-feed-mode)
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
+(defun set-evil-lookup-func ()
+  (setq-local evil-lookup-func
+	      (lambda () (describe-symbol (symbol-at-point)))))
+(add-hook 'emacs-lisp-mode-hook 'set-evil-lookup-func)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; keybindings
+(global-unset-key (kbd "C-M-i"))
+(global-set-key (kbd "C-M-i") 'company-complete)
