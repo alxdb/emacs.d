1fc9b325ae852fc80cbcebfb2e997325e9231a98
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-07 19:24:53 +0100

    Helm keybindings and comments

--- init.el
+++ init.el
@@ -1,8 +1,15 @@
+;; evil
 (require 'evil)
 (evil-set-initial-state 'vterm-mode 'emacs)
 
+;; projectile
 (require 'projectile)
 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
 
+;; helm
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x f") #'helm-find-files)
+
+;; load from custom.el
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
