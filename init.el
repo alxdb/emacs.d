3ff2ca4e168ba6fdec73ebd1107b17ea138cdd6d
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-08 00:13:23 +0100

    Python, eglot and other changes

--- init.el
+++ init.el
@@ -1,15 +1,22 @@
-;; evil
+;; Global
+(global-set-key (kbd "s-s") #'save-buffer)
+
+;; Evil
 (require 'evil)
 (evil-set-initial-state 'vterm-mode 'emacs)
 
-;; projectile
+;; Projectile
 (require 'projectile)
 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
 
-;; helm
+;; Helm
 (global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x f") #'helm-find-files)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
 
-;; load from custom.el
+;;; Python
+; use mypy via flycheck rather than eglot
+(add-hook 'python-mode 'flycheck-mode)
+
+;; Load customizations from custom.el
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
