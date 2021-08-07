be4dc2636d313ad9b01e06fab0eeb5294b2af768
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-07 16:40:49 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,26 @@
+(require 'evil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(custom-enabled-themes '(doom-vibrant))
+ '(custom-safe-themes t)
+ '(evil-mode t)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-selected-packages '(magit doom-themes evil))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
