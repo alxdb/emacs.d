97dad66108717305be33d4301a44b358d2d3e8e9
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-07 18:49:36 +0100

    Moved customizations into separate file

--- init.el
+++ init.el
@@ -1,49 +1,5 @@
 (require 'evil)
-
 (evil-set-initial-state 'term-mode 'emacs)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-save-default nil) 
- '(custom-enabled-themes '(doom-vibrant)) 
- '(custom-safe-themes t) 
- '(default-frame-alist '((width . 200) 
-			 (height . 60))) 
- '(evil-mode t) 
- '(evil-want-C-w-in-emacs-state t) 
- '(helm-mode t) 
- '(inhibit-startup-echo-area-message "alxdb") 
- '(inhibit-startup-screen t) 
- '(make-backup-files nil) 
- '(package-archives '(("gnu" . "https://elpa.gnu.org/packages/") 
-		      ("melpa" . "https://melpa.org/packages/"))) 
- '(package-selected-packages '(elisp-format try helm projectile magit doom-themes evil)) 
- '(scroll-bar-mode nil) 
- '(show-paren-mode t) 
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t 
-	     (:inherit nil 
-		       :extend nil 
-		       :stipple nil 
-		       :background "#242730" 
-		       :foreground "#bbc2cf" 
-		       :inverse-video nil 
-		       :box nil 
-		       :strike-through nil 
-		       :overline nil 
-		       :underline nil 
-		       :slant normal 
-		       :weight normal 
-		       :height 140 
-		       :width normal 
-		       :foundry "nil" 
-		       :family "Iosevka")))))
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
