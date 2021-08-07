9bd93fe3365f2583e200bf02793541294430dff3
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-07 18:26:45 +0100

    Ran elisp-format-buffer (pkg: elisp-format)
    - Also changed default font

--- init.el
+++ init.el
@@ -7,21 +7,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-default nil)
- '(custom-enabled-themes '(doom-vibrant))
- '(custom-safe-themes t)
- '(default-frame-alist '((width . 200) (height . 60)))
- '(evil-mode t)
- '(evil-want-C-w-in-emacs-state t)
- '(inhibit-startup-echo-area-message "alxdb")
- '(inhibit-startup-screen t)
- '(make-backup-files nil)
- '(package-archives
-   '(("gnu" . "https://elpa.gnu.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")))
- '(package-selected-packages '(projectile magit doom-themes evil))
- '(scroll-bar-mode nil)
- '(show-paren-mode t)
+ '(auto-save-default nil) 
+ '(custom-enabled-themes '(doom-vibrant)) 
+ '(custom-safe-themes t) 
+ '(default-frame-alist '((width . 200) 
+			 (height . 60))) 
+ '(evil-mode t) 
+ '(evil-want-C-w-in-emacs-state t) 
+ '(helm-mode t) 
+ '(inhibit-startup-echo-area-message "alxdb") 
+ '(inhibit-startup-screen t) 
+ '(make-backup-files nil) 
+ '(package-archives '(("gnu" . "https://elpa.gnu.org/packages/") 
+		      ("melpa" . "https://melpa.org/packages/"))) 
+ '(package-selected-packages '(elisp-format try helm projectile magit doom-themes evil)) 
+ '(scroll-bar-mode nil) 
+ '(show-paren-mode t) 
  '(tool-bar-mode nil))
 
 (custom-set-faces
@@ -29,4 +30,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t 
+	     (:inherit nil 
+		       :extend nil 
+		       :stipple nil 
+		       :background "#242730" 
+		       :foreground "#bbc2cf" 
+		       :inverse-video nil 
+		       :box nil 
+		       :strike-through nil 
+		       :overline nil 
+		       :underline nil 
+		       :slant normal 
+		       :weight normal 
+		       :height 140 
+		       :width normal 
+		       :foundry "nil" 
+		       :family "Iosevka")))))
