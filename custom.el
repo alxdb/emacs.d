0b4c708fc927106cd8bf93b6b273e5b1708d120d
Alexander Davidson Bryan <alxdb@pm.me>
2021-08-13 20:12:47 +0100

    More stuff!

--- custom.el
+++ custom.el
@@ -3,17 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-sources '("~/.authinfo"))
  '(auto-save-default nil)
- '(custom-enabled-themes '(doom-vibrant))
+ '(custom-enabled-themes '(doom-one))
  '(custom-safe-themes t)
  '(default-frame-alist '((width . 200) (height . 60)))
+ '(doom-modeline-mode t)
+ '(envrc-global-mode t)
  '(evil-mode t)
  '(evil-undo-system 'undo-fu)
  '(evil-want-C-w-in-emacs-state t)
+ '(flycheck-python-mypy-config '("mypy.ini" "setup.cfg" "pyproject.toml"))
  '(global-company-mode t)
+ '(helm-boring-buffer-regexp-list '("\\` " "\\`\\*" "magit:" "magit-process:"))
+ '(helm-ff-skip-boring-files t)
  '(helm-mode t)
  '(inhibit-startup-echo-area-message "alxdb")
  '(inhibit-startup-screen t)
+ '(initial-scratch-message nil)
  '(mac-command-modifier 'super)
  '(mac-option-modifier 'meta)
  '(make-backup-files nil)
@@ -21,12 +28,20 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
-   '(eglot flycheck poetry company markdown-mode which-key vterm undo-fu helm projectile magit doom-themes evil))
+   '(envrc forge doom-modeline eglot flycheck poetry company markdown-mode which-key vterm undo-fu helm projectile magit doom-themes evil))
  '(projectile-mode t nil (projectile))
  '(projectile-use-git-grep t)
  '(python-indent-def-block-scale 1)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]])
  '(tool-bar-mode nil)
  '(which-key-mode t))
 
--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;; Evil
 (require 'evil)
 (evil-set-initial-state 'vterm-mode 'emacs)
+(evil-set-initial-state 'xref--xref-buffer-mode 'emacs)
 
 ;; Projectile
 (require 'projectile)
@@ -12,11 +13,29 @@
 ;; Helm
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x b") #'helm-buffers-list)
+
+;; Magit
+(with-eval-after-load 'magit
+  (require 'forge))
+
+;; Eglot
+(defun format-buffer-before-save ()
+  (add-hook 'before-save-hook #'eglot-format-buffer nil t))
 
 ;;; Python
-; use mypy via flycheck rather than eglot
-(add-hook 'python-mode 'flycheck-mode)
+(defun alxdb/python-hook ()
+  (eglot-ensure)
+  (flycheck-mode) ; https://github.com/Richardk2n/pylsp-mypy/issues/16
+  (set-fill-column 99)
+  (display-fill-column-indicator-mode)
+  (format-buffer-before-save)
+  (when (executable-find "ipython")
+    (setq python-shell-interpreter "ipython")))
+
+(add-hook 'python-mode-hook #'alxdb/python-hook)
 
 ;; Load customizations from custom.el
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
+(server-start)
